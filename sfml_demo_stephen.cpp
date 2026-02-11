ca74b5c020642ec2b76c8b848f3da3409175364c
stephent09876 <stephenthorsell@gmail.com>
2026-02-10 22:02:38 -0600

    sfml won't have issues with creating animations

--- sfml_demo_stephen.cpp
+++ sfml_demo_stephen.cpp
@@ -4,7 +4,8 @@
  * Description: This file demos the ability to create a window, draw a circle, 
  * and sometime later, delete the circle and draw a new circle somewhere else.
  * The purpose of this is to confirm that we can actually do an animation by 
- * updating the "frame" of the animation.
+ * updating the "frame" of the animation. This is a rendition of the classic
+ *  DVD logo bouncing off of the walls of the window (DO NOT RESIZE WINDOW LIVE)
  * 
  *******************************************************************************/
 
@@ -13,12 +14,27 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({1300,800}), "SFML works!");
+    unsigned int window_length = 1300;
+    unsigned int window_height = 800;
+    float r = 5.0f;     // circle radius
 
-    window.setPosition(sf::Vector2i(10, 10)); // positioning window closer to the corner of the screen
-    window.setFramerateLimit(30);   // setting frame rate 
-    sf::CircleShape shape(10.f);
+
+    sf::RenderWindow window(sf::VideoMode({window_length, window_height}), 
+                                            "Animation demo works!");
+
+    // positioning window closer to the corner of the screen
+    window.setPosition(sf::Vector2i(10, 10)); 
+    window.setFramerateLimit(60);   // setting frame rate 
+    sf::CircleShape shape(r);
+    shape.setPosition(sf::Vector2f(0.0, 0.0));
     shape.setFillColor(sf::Color::White);
+
+    // position variables to update
+    float x_pos = 0.0;
+    float y_pos = 0.0;
+
+    float x_vel = 2.0;
+    float y_vel = 2.0;
 
     while (window.isOpen())
     {
@@ -30,8 +46,23 @@ int main()
             }
         }
 
+
+        // draw shape
         window.clear();
+        shape.setPosition(sf::Vector2f(x_pos, y_pos));
         window.draw(shape);
         window.display();
+
+        // update velocity if circle bounces of boundary
+        if (x_pos < 0.0 || x_pos > (static_cast<float>(window_length) - 2.0*r)) {
+            x_vel = -1.0 * x_vel;
+        }
+        if (y_pos < 0.0 || y_pos > (static_cast<float>(window_height) - 2.0*r)) {
+            y_vel = -1.0 * y_vel;
+        }
+
+        // update animation dynamics
+        x_pos = x_pos + x_vel;
+        y_pos = y_pos + y_vel;
     }
 }
