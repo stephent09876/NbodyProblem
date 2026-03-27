e4f45e900d263911257b310ee475d5d73bb45403
Elijah Salandanan <78926681+sawl-an-dawnen@users.noreply.github.com>
2026-03-26 19:39:48 -0500

    adding particle id and log function. Test log function in eulerstep integrator.

--- src/Integrator.cpp
+++ src/Integrator.cpp
@@ -61,6 +61,7 @@ void Integrator::EulerStep(float dt) {
     for (Particle &p : gravity.p) {
         p.velocity += p.accel * dt;
         p.position += p.velocity * dt;
+        p.log();
     }
 }
 
